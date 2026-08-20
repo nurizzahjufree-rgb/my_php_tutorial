ae9c125f1b1f701319066a450f8575e08a177b65
nurizzahjufree-rgb <nurizzah.jufree@student.gmi.edu.my>
2026-08-21 00:35:02 +0800

    Commiting my first project

---
+++ video_4.php
@@ -0,0 +1,26 @@
+<?php 
+
+	define('NAME', 'Izzah');
+
+	// $name = 'Izzah';
+	$age = 20;
+
+	// $name = 'Mario';
+	
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Tutorials</title>
+</head>
+<body>
+
+	<h1>User profile page</h1>
+
+	<div><?php echo NAME; ?></div>
+	<div><?php echo $age; ?></div>
+
+</body>
+</html>
