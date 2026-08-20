7807855c00d8e3487faa53c3187124bcc0c73769
nurizzahjufree-rgb <nurizzah.jufree@student.gmi.edu.my>
2026-08-21 00:44:08 +0800

    my notes for video 1

---
+++ video_1.php
@@ -0,0 +1,10 @@
+//WHY LEARN PHP?
+
+// - popular
+// - easy to pick up & run with
+// - huge and active community for support
+// - still a shed-load of of PHP jobs on the market
+
+//PHP Hypertext Preprocessor
+
+//PHP is a server-side scripting language
