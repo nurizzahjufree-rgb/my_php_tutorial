36ebea440553fac72056150c2717133965d0f3dc
nurizzahjufree-rgb <nurizzah.jufree@student.gmi.edu.my>
2026-08-28 17:02:46 +0800

    Rename video_3.php

--- templates/header.php
+++ templates/header.php
@@ -14,9 +14,9 @@
 <body class="grey lighten-4">
 	<nav class="white z-depth-0">
     <div class="container">
-      <a href="#" class="brand-logo brand-text">Izzah Pizzas</a>
+      <a href="add.php" class="brand-logo brand-text">Izzah Pizzas</a>
       <ul id="nav-mobile" class="right hide-on-small-and-down">
-        <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+        <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
       </ul>
     </div>
   </nav>
